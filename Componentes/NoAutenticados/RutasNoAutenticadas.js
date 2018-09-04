1a4196fc942d802f4ab4a22d4dd784ecdae0d680
Leydy <leydymayumy@gmail.com>
2018-09-04 13:41:59 -0500

    Navegación entre 'StackNavigator'

--- Componentes/NoAutenticados/RutasNoAutenticadas.js
+++ Componentes/NoAutenticados/RutasNoAutenticadas.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { Text, Button, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
 const SignIn = (props) => {
   console.log(props);
-  return <Text>Componente SignIn</Text>;
+  // props.navigation.navigate();
+  const { navigation } = props;
+  return (
+    <View>
+      <Text>Componente SignIn</Text>
+      <Button
+        title="Navegar a SignUp"
+        onPress={() => {
+          navigation.navigate('SignUp');
+        }}
+      />
+    </View>
+  );
 };
-const SignUp = () => <Text>Componente SignUp</Text>;
+const SignUp = (props) => {
+  const { navigation } = props;
+  return (
+    <View>
+      <Text>Componente SignUp</Text>
+      <Button
+        title="Regresar"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
+};
 
-const RutasNoAutenticadas = StackNavigator({
+const RutasNoAutenticadas = createStackNavigator({
   SignIn: {
     screen: SignIn,
   },

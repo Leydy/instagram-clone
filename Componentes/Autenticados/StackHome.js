b21430a5fc60ca5c79e83578d9cb1d86a6dabaa2
Leydy <leydymayumy@gmail.com>
2018-09-12 19:10:57 -0500

    StackHome

---
+++ Componentes/Autenticados/StackHome.js
@@ -0,0 +1,23 @@
+import { StackNavigator } from 'react-navigation';
+
+import Home from './Home';
+import Autor from './Profile';
+import Publicacion from './Publicacion';
+import Comentarios from './Comentarios';
+
+const StackHome = StackNavigator({
+  Home: {
+    screen: Home,
+  },
+  Autor: {
+    screen: Autor,
+  },
+  Publicacion: {
+    screen: Publicacion,
+  },
+  Comentarios: {
+    screen: Comentarios,
+  },
+});
+
+export { StackHome };

8953979fc7d944a443cca94903c21c758f9cb1a2
Leydy <leydymayumy@gmail.com>
2018-10-11 21:00:01 -0500

    Integrando firebase

---
+++ Store/Servicios/Firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyCW076flkUwcx8H5wRQn_70nuQAFGjGwfs',
+  authDomain: 'instagram-clone-5cdce.firebaseapp.com',
+  databaseURL: 'https://instagram-clone-5cdce.firebaseio.com',
+  projectId: 'instagram-clone-5cdce',
+  storageBucket: 'instagram-clone-5cdce.appspot.com',
+  messagingSenderId: '1045051163801'
+};
+firebase.initializeApp(config);

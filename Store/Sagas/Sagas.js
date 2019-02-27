45fb11d248e9d879b756b1ff86cf2d01cb8f4dbc
Leydy <leydymayumy@gmail.com>
2019-02-27 18:49:19 -0500

    Redux-saga effects takeEvery, call

--- Store/Sagas/Sagas.js
+++ Store/Sagas/Sagas.js
@@ -1,3 +1,25 @@
-export default function* functionPrimaria() {
-  console.log('Desde funcion generadora');
+import { takeEvery, call } from 'redux-saga/effects';
+import { autenticacion } from '../Servicios/Firebase';
+
+const registroEnFirebase = values => autenticacion
+  .createUserWithEmailAndPassword(values.correo, values.password)
+  .then((success) => {
+    console.log(success);
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    // ...
+  });
+
+function* generadoraRegistro(values) {
+  yield call(registroEnFirebase, values.datos);
+  console.log(values);
+}
+export default function* funcionPrimaria() {
+  yield takeEvery('REGISTRO', generadoraRegistro)
+  console.log('Desde nuestra función generadora');
 }
